b1b254d57652a78730ce6c24b8e98b5a1bd0baa6
171G0574@rcarbonifera.tecnm.mx <171G0574@rcarbonifera.tecnm.mx>
2020-09-15 17:47:01 -0500

    Actividad2

---
+++ PRACTICA 1 UNIDAD 2/Controllers/HomeController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using PRACTICA_1_UNIDAD_2.Models;
+namespace PRACTICA_1_UNIDAD_2.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+
+            mexicoContext context = new mexicoContext();
+            var estados = context.Estados.OrderBy(x => x.Nombre).ToList();
+
+
+            return View(estados);
+        }
+    }
+}
---
+++ PRACTICA 1 UNIDAD 2/Models/Estados.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace PRACTICA_1_UNIDAD_2.Models
+{
+    public partial class Estados
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public string Abrev { get; set; }
+    }
+}
